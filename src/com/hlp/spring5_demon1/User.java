0c629e3c7136cb4e39b6b39b0be855e65276801a
hanlipeng <1103953171@qq.com>
2022-03-20 20:10:26 +0000

    origin

--- src/com/hlp/spring5_demon1/User.java
+++ src/com/hlp/spring5_demon1/User.java
@@ -6,17 +6,6 @@ package com.hlp.spring5_demon1;
  */
 public class User {
     public void add(){
-        System.out.println("add...");//啊啦啦啦啦啦...
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("hot-fix test");//pull test
-        //我来测试一下pullbbbb
-        //我来测试一下pullbbbb
-        //我来测试一下pullbbbb
-        //我来测试一下pullbbbb
+        System.out.println("add...");
     }
 }
