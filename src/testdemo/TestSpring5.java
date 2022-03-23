a4ee2132052d73928aac3704da46ae0b48a4c5e2
hanlipeng <1103953171@qq.com>
2022-03-23 23:36:44 +0000

    3.23

--- src/testdemo/TestSpring5.java
+++ src/testdemo/TestSpring5.java
@@ -2,6 +2,7 @@ package testdemo;
 
 import com.hlp.spring5_demon1.User;
 import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
@@ -15,12 +16,11 @@ public class TestSpring5 {
     public void testAdd(){
         //1.加载spring配置文件
         //ClassPathXmlApplicationContext是类路径，在src下。另一种方法：FileSystemXmlApplicationContext
-        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+        BeanFactory context = new ClassPathXmlApplicationContext("bean1.xml");
 
         //2.获取配置创建的对象
         User user = context.getBean("user", User.class);//user是别名，体现在xml文件的<bean>标签中的id上
         System.out.println(user);
         user.add();
-
     }
 }
